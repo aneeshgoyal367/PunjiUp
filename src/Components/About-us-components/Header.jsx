a7ef2b47194beae0b234d8c1604219d214fca5cd
aneeshgoyal367 <aneeshgoyal367@gmail.com>
2021-08-05 16:01:34 +0530

    styling done

--- src/Components/About-us-components/Header.jsx
+++ src/Components/About-us-components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Header() {
     return (
         <div >
-            <img
+            {/* <img
                 alt=""
                 src="assets\photos\logo1.png"
                 width="150"
@@ -14,7 +14,14 @@ function Header() {
           
                 
                 <Link to="/Contactus" className="links" >Contact Us</Link>
-                <Link to="/" className="links" >Home</Link>
+                <Link to="/" className="links" >Home</Link> */}
+                <ul id="menu2">
+            <img src="assets/photos/logo.png" class="logo2" alt="logo" width="300" height="150" />
+
+
+            <li><Link to="/Contactus">Contact Us</Link></li>
+                <li><Link to="/">Home</Link></li>
+            </ul>
                 
            
         </div>
--- src/Components/contact-us-components/Header.jsx
+++ src/Components/contact-us-components/Header.jsx
@@ -3,7 +3,7 @@ import {Link } from 'react-router-dom'
 function Header() {
     return (
         <div >
-            <img
+            {/* <img
                 alt=""
                 src="assets\photos\logo1.png"
                 width="150"
@@ -14,7 +14,14 @@ function Header() {
           
                 <Link to="/Aboutus" className="links">About Us</Link>
                 
-                <Link to="/" className="links" >Home</Link>
+                <Link to="/" className="links" >Home</Link> */}
+                <ul id="menu1">
+            <img src="assets/photos/logo.png" class="logo1" alt="logo" width="300" height="150" />
+
+
+                <li><Link to="/Aboutus">About Us</Link></li>
+                <li><Link to="/">Home</Link></li>
+            </ul>
                 
             
         </div>
