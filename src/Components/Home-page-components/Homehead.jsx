be31c530d21be6ee324c480355fe68bedb8de262
aneeshgoyal367 <aneeshgoyal367@gmail.com>
2021-08-05 14:29:08 +0530

    styling done

--- src/Components/Home-page-components/Homehead.jsx
+++ src/Components/Home-page-components/Homehead.jsx
@@ -3,14 +3,18 @@ import {Link} from 'react-router-dom'
 function Homehead() {
     return (
         <div>
-            <table>
+            {/* <table>
         <tr>
-            <th><img src="assets/photos/logo.png" class="logo" alt="logo" width="300" height="150" /></th>
+            <th> */}
             <ul id="menu">
+            <img src="assets/photos/logo.png" class="logo" alt="logo" width="300" height="150" />
+            
+            
+           
           
 
-             {/* <!-- <li><img src="cart.png" class="cart" width="50" height="50"></li>
-                <li>Buy</li> --> */}
+             {/* {/* {/* <!-- <li><img src="cart.png" class="cart" width="50" height="50"></li> */}
+                {/* <li>Buy</li> --> */}
 
                 <li><Link to="/Signuppage">SignUp</Link></li>
                 <li><Link to="/Signinpage">SignIn</Link></li>
@@ -19,9 +23,8 @@ function Homehead() {
                 <li><Link to="/Aboutus">About Us</Link></li>
                 <li><Link to="/Contactus">Contact Us</Link></li>
             </ul>
-        </tr>
-    </table>
+       
         </div>
     )
 }
-export default Homehead
+export default Homehead 
