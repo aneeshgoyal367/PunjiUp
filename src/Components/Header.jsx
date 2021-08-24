4b18af308e1454dbc58036b06012f659ca606675
Singh-Neelmani <neelmani.singh@timesinternet.in>
2021-08-24 18:41:00 +0530

    search Updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,40 +1,65 @@
-import React, { useRef, useEffect, useState } from 'react'
-import { Route, useHistory,Redirect } from 'react-router-dom'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
-import Result from './SearchResult/Result'
+import React, { useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../CSS/Header.css'
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'http://localhost:3000/JSON/Search.json'
+    baseURL: 'http://localhost:8080/api/home'
 })
 function Header(props) {
+
     const inputResort = useRef();
+    const inputResort1 = useRef();
     let history = useHistory();
     const [searchData, setsearchData] = useState([]);
     const [showList, setshowList] = useState(false);
+    const [info, setInfo] = useState("search_fund_manager");
     const [name, setname] = useState();
-    useEffect(() => {
-        api.get('').then(res => {
-            setsearchData(res.data)
-        })
-    }, [])
+
     function removeuser() {
         localStorage.removeItem('userEmailid')
         history.push('/InvSignin')
     }
-    function searching(event) {
-        if (event.target.value.length) {
+    const searching1 = (data) => {
+        setInfo(data.target.value);
+    }
+    const searching = (event) => {
+        if (info === "search_fund_manager" && event.target.value.length) {
             setshowList(true);
             setname(event.target.value)
+            api.get(`/fundManager?name=${event.target.value}`).then(res => {
+                if (res && res.data && res.data.length) {
+                    setsearchData(res.data)
+                } else {
+                    setshowList(false);
+                    setsearchData([])
+                }
+
+            })
         }
-        else {
+        else if (info === "search_fund" && event.target.value.length) {
+            setshowList(true);
+            setname(event.target.value)
+            api.get(`/fundList?name=${event.target.value}`).then(res => {
+                if (res && res.data && res.data.length) {
+                    setsearchData(res.data)
+                } else {
+                    setshowList(false);
+                    setsearchData([])
+                }
+
+            })
+        } else {
+            setname("")
             setshowList(false);
+            setsearchData([])
         }
     }
+
     function finalResult(event) {
         history.push({
             pathname: '/searchresult',
-            state: { data:name }
+            state: { data: name }
         });
     }
     return (
@@ -54,13 +79,23 @@ function Header(props) {
             {props.home ? <Link to="/Home" className="links" >Home</Link> : ""}
             {props.search ? <form action="/" className="links" onSubmit={finalResult}>
                 <label htmlFor="header-search"></label>
-                <input ref={inputResort} type="text" list="tahoe-resorts" className="datalist" value={name} onChange={searching} />
-                {showList ?
+                <select onChange={(e) => searching1(e)} id="dropdown">
+                    <option value="search_fund_manager">Fund Manager</option>
+                    <option value="search_fund">Fund</option>
+                </select>
+
+                <input ref={inputResort} type="text" list="tahoe-resorts" className="datalist" value={name} onChange={searching} id="searchbox" />
+                {showList && searchData.length && info === "search_fund_manager" ?
                     <datalist id="tahoe-resorts" >
                         {searchData.map((item, key) =>
-                            <option key={key} value={item.name} />
+                            <option key={key} value={item.firstName} />
                         )}
-                    </datalist> : ""}
+                    </datalist>
+                    : <datalist id="tahoe-resorts" >
+                        {searchData.map((item, key) =>
+                            <option key={key} value={item.fundName} />
+                        )}
+                    </datalist>}
                 <button type="submit">Search</button>
             </form> : ""}
         </div>
