3dbbc4395c6fccc9907b1e13e4204b519a1145d1
aneeshgoyal367 <aneeshgoyal367@gmail.com>
2021-08-26 00:06:31 +0530

    some change in approve

--- src/Components/manager-profile-components/approve.jsx
+++ src/Components/manager-profile-components/approve.jsx
@@ -1,75 +1,45 @@
 import React, { useState, useEffect } from 'react'
+// import Show from './Show';
+
+import GridComponent from '../open-end-components/Grid-Component';
+
 import axios from 'axios'
-
-
 const api = axios.create({
     baseURL: 'https://punjiup.herokuapp.com/api/fundmanager'
-  })
-const Table = () => {
-    const [investors, setInvestors] = useState([])
+})
+function Approve() {
 
+    const [respData, setData] = useState([])
+    let gridProp = {
+        
+        columnHeaderNames: [
+            "Transaction Id",
+            "Transaction Status"
+           
+        ],
+        data: respData
+    
+    };
     useEffect(() => {
-        getData()
+        api.get('/fund/transactions').then(res => {
+            setData(res.data);
+            gridProp.data = res.data;
+        })
     }, [])
 
-    const getData = async () => {
 
-        const response = await axios.get(URL)
-        setInvestors(response.data)
-    }
-
-    const removeData = (id) => {
-
-        axios.delete(`${URL}/${id}`).then(res => {
-            const del = investors.filter(investor => id !== investor.id)
-            setInvestors(del)
-        })
-    }
-
-    const renderHeader = () => {
-        // let headerElement = ['id', 'Fund Name', 'Investor EmailId', 'operation']
-        let headerElement = ['Transaction Id', 'Transaction Status']
-        return headerElement.map((key, index) => {
-            return <th key={index}>{key.toUpperCase()}</th>
-        })
-    }
-    api.get('/fund/transactions', body, { "headers": { "content-type": "application/json", } })
-    .then(res=>{
-      console.log(res.data)
-    })
-   }
-
-    const renderBody = () => {
-        return investors && investors.map(({ id, name, email }) => {
-            return (
-                <tr key={id}>
-                    <td>{id}</td>
-                    {/* <td>{name}</td>
-                    <td>{email}</td> */}
- <td className='opration'>
-                        <button className='button' onClick={() => removeData(id)}>Reject</button>
-                    </td>
-                </tr>
-            )
-        })
-    }
-
+    
     return (
-        <>
-            <h1 id='title'>Approve/Reject Funds Below</h1>
-            <table id='investor'>
-                <thead>
-                    <tr>{renderHeader()}</tr>
-                </thead>
-                <tbody>
-                    {renderBody()}
-                </tbody>
-            </table>
-        </>
+
+        // data.map((e) => {
+        //     return <Show show={e} />
+
+        // })
+        (gridProp && gridProp.data) &&
+        <GridComponent gridObj={gridProp} />
+
     )
 }
-
-
-export default Table
+export default Approve
 
 
